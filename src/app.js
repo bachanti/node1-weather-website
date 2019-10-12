8b3a9d2af6ab7f5c71905e487dc04ede9d1a3c35
Bhaskar Achanti <Bhaskar@Bhaskars-MacBook-Pro.local>
2019-10-12 10:33:10 +0300

    moving the app.js changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express') /* hi */
 const path = require('path')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode.js')
